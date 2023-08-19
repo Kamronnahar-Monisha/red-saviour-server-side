5ae2070e0b08be13b37008138e61f15ae75ad3d8
Kamronnahar-Monisha <kamronnaharmonisha12@gmail.com>
2023-08-19 11:22:23 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 const app = express();
@@ -11,20 +12,38 @@ app.use(cors());
 app.use(express.json());
 
 
-//post api for adding a user
-app.post('/users', async (req, res) => {
-    const user = req.body;
-    const email = user.email;
-    const query = { email };
-    const userFindResult = await usersCollection.findOne(query);
-    if (!userFindResult) {
-        const result = await usersCollection.insertOne(user);
-        res.send(result);
+//mongodb connection uri and client
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.nomds.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+// async function for CRUD operation
+const run = async () => {
+    try {
+        const usersCollection = client.db('red-saviour').collection('users');
+
+
+        //post api for adding a user
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const email = user.email;
+            const query = { email };
+            const userFindResult = await usersCollection.findOne(query);
+            if (!userFindResult) {
+                const result = await usersCollection.insertOne(user);
+                res.send(result);
+            }
+            else {
+                res.send({});
+            }
+        });
+
     }
-    else {
-        res.send({});
+    finally {
+
     }
-});
+}
+run().catch(console.dir);
 
 
 
